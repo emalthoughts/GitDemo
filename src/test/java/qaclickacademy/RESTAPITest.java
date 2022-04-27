4a8d53d1c9b040205844eae48909dde4a94abdeb
emal <emalrahimi86@gmail.com>
2022-04-27 16:37:10 -0400

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,31 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	
+	@Test
+	public void postJira()
+	{
+		
+		System.out.println("postJira");
+		
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		
+		System.out.println("postdeleteTwitterJira");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,34 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	//testing xml file in Maven
+	//how to execute all test cases from Test Folder - mvn test
+
+	@Test
+	public void BrowserAutomation()
+	{
+		
+		System.out.println("BrowserAutomation");
+		
+	}
+	
+	
+	
+	@Test
+	public void elementsUI()
+	{
+		
+		System.out.println("elementsUI");
+		
+	}
+	
+	
+	
+	
+	
+	
+}
